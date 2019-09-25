fc190e0b8674e8584688fb63d128362718c69378
Tun Lin Thu <tun.lin.thu@persol.co.jp>
2019-09-25 11:05:38 +0900

    commit

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('db.json');
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 3000;
 
-server.use(middlewares);
-server.use(router);
+// server.use(middlewares);
+// server.use(router);
 
-server.listen(port);
+// server.listen(port);
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use('/static', express.static('public'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
